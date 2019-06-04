74750fd1db879ca5f2c4f7bdbb0bd65c043b5735
Younes Abdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2019-06-04 17:43:06 +0200

    #3 #8 Added functions for Queue model. Managing the queue is no longer the responsability of real-time-appointment

--- back/app/api/index.js
+++ back/app/api/index.js
@@ -5,6 +5,7 @@ const ApplicationRouter = require('./application');
 const ConnectionRouter = require('./connection');
 const AvailabilitiesRouter = require('./availabilities');
 const RealTimeAppointmentRouter = require('./real-time-appointment');
+const QueueRouter = require('./queue');
 
 const router = new Router();
 router.get('/status', (req, res) => res.status(200).json('ok'));
@@ -14,5 +15,6 @@ router.use('/appointments', AppointmentRouter);
 router.use('/connection', ConnectionRouter);
 router.use('/availabilities', AvailabilitiesRouter);
 router.use('/real-time-appointment', RealTimeAppointmentRouter);
+router.use('/queue', QueueRouter);
 
 module.exports = router;
