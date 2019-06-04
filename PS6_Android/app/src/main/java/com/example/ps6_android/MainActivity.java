19d7c3d3851b2a487ed45feb0ecaa7e008e653a3
ColombanThomas <thomas.colomban@etu.unice.fr>
2019-06-04 18:58:59 +0200

    App Student side

--- PS6_Android/app/src/main/java/com/example/ps6_android/MainActivity.java
+++ PS6_Android/app/src/main/java/com/example/ps6_android/MainActivity.java
@@ -1,32 +1,25 @@
 package com.example.ps6_android;
 
+import android.graphics.drawable.Drawable;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.example.ps6_android.Models.Applicant;
 import com.example.ps6_android.Models.Appointment;
 
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    public MqttAndroidClient mqttAndroidClient;
 
     Applicant applicant;
 
@@ -51,5 +44,40 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        final TextView identifiant = findViewById(R.id.identifiant);
+        final TextView bri1 = findViewById(R.id.bri);
+        final TextView bri2 = findViewById(R.id.bri2);
+        final TextView objet = findViewById(R.id.objet2);
+        final TextView heure = findViewById(R.id.heure2);
+        final TextView description = findViewById(R.id.description2);
+
+
+        final RelativeLayout linearLayout = findViewById(R.id.linearLayout);
+
+
+
+        boolean retard = false;
+
+        if(!retard){
+            Drawable drawablePic = getResources().getDrawable(R.drawable.rectangle_green);
+            bri1.setText("Le BRI est disponible");
+            bri2.setText("Vous pouvez vous rendre au bureau maintenant");
+            heure.setText("Lundi 3 juin: 15h45");
+            objet.setText("Remise du dossier de candidature");
+            description.setText("Je veux mettre mon dossier et poser quelques questions");
+            linearLayout.setBackground(drawablePic);
+
+        }else{
+            Drawable drawablePic = getResources().getDrawable(R.drawable.rectangle_orange);
+            bri1.setText("RETARD: RDV décalé à 15h55");
+            bri2.setText("Le BRI vous recevra avec du retard");
+            heure.setText("Lundi 3 juin: 15h45");
+            objet.setText("Remise du dossier de candidature");
+            description.setText("Je veux mettre mon dossier et poser quelques questions");
+            linearLayout.setBackground(drawablePic);
+        }
+
     }
 }
