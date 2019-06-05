e3a778a864e3e709261b3aeae3a1883385e211d1
ColombanThomas <thomas.colomban@etu.unice.fr>
2019-06-05 16:14:45 +0200

    Refactoring layout

--- PS6_Android/app/src/main/java/com/example/ps6_android/MainActivity.java
+++ PS6_Android/app/src/main/java/com/example/ps6_android/MainActivity.java
@@ -26,16 +26,8 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
 
-    Applicant applicant;
-    TextView identifier;
-    TextView bri1;
-    TextView bri2;
-    TextView object;
-    TextView hour;
-    TextView description;
 
-    TextView identificationFirstPers;
-    TextView identificationFirstPers2;
+    TextView idStudent;
 
     RelativeLayout linearLayout;
 
@@ -51,27 +43,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        idStudent = findViewById(R.id.idStudent);
+
         receiver = new MqttDataReceiver();
         this.registerReceiver(receiver, new IntentFilter("DATA_CHANGED"));
-        identifier = findViewById(R.id.identifier);
-        bri1 = findViewById(R.id.bri);
-        bri2 = findViewById(R.id.bri2);
-        object = findViewById(R.id.object2);
-        hour = findViewById(R.id.hour2);
-        description = findViewById(R.id.description2);
-
-        identificationFirstPers = findViewById(R.id.identificationFirstPers);
-        identificationFirstPers2 = findViewById(R.id.identificationFirstPers2);
-
-        linearLayout = findViewById(R.id.linearLayout);
-
-        changeViewButton = findViewById(R.id.changeView);
-
-
 
         startService(new Intent(this, MqttService.class));
 
-        final RelativeLayout linearLayout = findViewById(R.id.linearLayout);
 
         WebHelper.getAppointments("40", new WebHelper.CallBack() {
             @Override
@@ -90,48 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-        boolean retard = false;
-        boolean disponible = false;
-
-        identifier.setText("57");
-
-        if(!retard && disponible){
-            Drawable drawablePic = getResources().getDrawable(R.drawable.rectangle_green);
-            bri1.setText("Le BRI est disponible");
-            bri2.setText("Vous pouvez vous rendre au bureau maintenant");
-            hour.setText("Lundi 3 juin: 15h45");
-            object.setText("Remise du dossier de candidature");
-            description.setText("Je veux mettre mon dossier et poser quelques questions");
-            linearLayout.setBackground(drawablePic);
-
-        }else if (retard && !disponible){
-            Drawable drawablePic = getResources().getDrawable(R.drawable.rectangle_orange);
-            bri1.setText("RETARD: RDV décalé à 15h55");
-            bri2.setText("Le BRI vous recevra avec du retard");
-            hour.setText("Lundi 3 juin: 15h45");
-            object.setText("Remise du dossier de candidature");
-            description.setText("Je veux mettre mon dossier et poser quelques questions");
-            linearLayout.setBackground(drawablePic);
-        }else if (!retard && !disponible){
-            Drawable drawablePic = getResources().getDrawable(R.drawable.rectangle);
-            bri1.setText("Ce n'est pas votre tour");
-            bri2.setText("Pas de retard à signaler");
-            hour.setText("Lundi 3 juin: 15h45");
-            object.setText("Remise du dossier de candidature");
-            description.setText("Je veux mettre mon dossier et poser quelques questions");
-            identificationFirstPers.setText("Passage en cours: ");
-            identificationFirstPers2.setText("Aucun");
-            linearLayout.setBackground(drawablePic);
-        }
-        changeViewButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), BRIActivity.class));
-            }
-        });
-
-
     }
 
     /**
@@ -140,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     public void updateView(String id){
         Log.d("update",id+"");
         currentId = id;
-        identificationFirstPers2.setText(id);
+        idStudent.setText(id);
     }
 
     public void updateAppointment(String json){
